99410e9105d47fc26dc08f710bcf2a614ec91a38
Natasha Sanjrani <44466954+nsanjrani@users.noreply.github.com>
2024-05-26 21:20:37 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='prot_lig_combine',
+   version='1.0.0',
+   description='Combine protein and ligand files',
+   url='https://github.com/gsk-tech/prot-lig-combine',
+   author = 'Natasha Sanjrani',
+   packages=find_packages()
+)
+
